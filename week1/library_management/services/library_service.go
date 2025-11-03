09b2c69d7e1ac29ee909ce6a3b3a5a654e0b28c5
Sofinaye <abene.fekadu@gmail.com>
2025-11-03 20:16:39 +0200

    Interface implementation added

--- week1/library_management/services/library_service.go
+++ week1/library_management/services/library_service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"errors"
+	"fmt"
 	"library_management/models"
 )
 
@@ -37,4 +39,39 @@ func (l *Library) GetMember(id int) (models.Member, bool) {
 func (l *Library) GetBook(id int) (models.Book, bool) {
 	b, ok := l.books[id]
 	return b, ok
+}
+
+func (l *Library) AddBook(book models.Book) {
+	// If not specified, default to Available
+	if book.Status == "" {
+		book.Status = "Available"
+	}
+	l.books[book.ID] = book
+}
+
+func (l *Library) RemoveBook(bookID int) {
+	delete(l.books, bookID)
+}
+
+func (l *Library) BorrowBook(bookID int, memberID int) error {
+	book, ok := l.books[bookID]
+	if !ok {
+		return fmt.Errorf("book with ID %d not found", bookID)
+	}
+	member, ok := l.members[memberID]
+	if !ok {
+		return fmt.Errorf("member with ID %d not found", memberID)
+	}
+
+	if book.Status != "Available" {
+		return errors.New("book is not available")
+	}
+
+	book.Status = "Borrowed"
+	l.books[bookID] = book
+
+	// Append to member's borrowed list.
+	member.BorrowedBooks = append(member.BorrowedBooks, book)
+	l.members[memberID] = member
+	return nil
 }
