d2aec123ac7f51a349ba130218fd01e1517154b0
Sofinaye <abene.fekadu@gmail.com>
2025-11-12 20:41:09 +0200

    Post function Added

--- week2/web-service-gin/main.go
+++ week2/web-service-gin/main.go
@@ -16,6 +16,7 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -28,4 +29,18 @@ var albums = []album{
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
