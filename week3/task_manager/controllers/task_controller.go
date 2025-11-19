b6113ab233ad91e4268d95df4086ff2b7abd0b63
Sofinaye <abene.fekadu@gmail.com>
2025-11-19 21:32:33 +0200

    Get tasks

--- week3/task_manager/controllers/task_controller.go
+++ week3/task_manager/controllers/task_controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
 	"task_manager/data"
+
+	"github.com/gin-gonic/gin"
 )
 
 type TaskController struct {
@@ -10,4 +13,10 @@ type TaskController struct {
 
 func NewTaskController(service *data.TaskService) *TaskController {
 	return &TaskController{service: service}
+}
+func (tc *TaskController) GetTasks(c *gin.Context) {
+	tasks := tc.service.GetAllTasks()
+	c.JSON(http.StatusOK, gin.H{
+		"data": tasks,
+	})
 }
