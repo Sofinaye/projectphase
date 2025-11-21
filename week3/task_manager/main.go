835d6204cca3b1fbf89fa0e4ebc6336d7c7ef6c0
Sofinaye <abene.fekadu@gmail.com>
2025-11-21 21:17:26 +0200

    Added MongoDB client initialization, collections setup and server start setup

--- week3/task_manager/main.go
+++ week3/task_manager/main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
 	"task_manager/data"
 	"task_manager/router"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	taskService := data.NewTaskService()
+	// MongoDB connection string (can be set via env var)
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
+
+	// Database and collection names
+	dbName := "task_manager_db"
+	tasksCollName := "tasks"
+	countersCollName := "counters"
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOpts := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			log.Printf("Error disconnecting MongoDB client: %v", err)
+		}
+	}()
+
+	// Ping MongoDB to verify connection
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	log.Println("Connected to MongoDB at", mongoURI)
+
+	db := client.Database(dbName)
+	tasksColl := db.Collection(tasksCollName)
+	countersColl := db.Collection(countersCollName)
+
+	taskService := data.NewTaskService(tasksColl, countersColl)
 	r := router.SetupRouter(taskService)
 
 	log.Println("Task Management API is running on http://localhost:8080")
